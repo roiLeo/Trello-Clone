24e052566e84d0e1b4cc4ae393dad9d5c6494336
Nobody <medina.leo42@gmail.com>
2017-03-14 11:15:32 +0100

    FontAwesome

--- src/AppBundle/Resources/public/js/app.js
+++ src/AppBundle/Resources/public/js/app.js
@@ -1,15 +1,15 @@
 /**
  * Created by Utilisateur on 13/03/2017.
  */
-$(document).on('click', '.panel-heading span.clickable', function(e){
+$(document).on('click', '.card-header span.clickable', function(e){
     var $this = $(this);
-    if(!$this.hasClass('panel-collapsed')) {
-        $this.parents('.panel').find('.panel-body').slideUp();
-        $this.addClass('panel-collapsed');
-        $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+    if(!$this.hasClass('collapsed')) {
+        $this.parents('.card').find('.card-block').slideUp();
+        $this.addClass('collapsed');
+        $this.find('i').removeClass('fa-chevron-up').addClass('fa-chevron-down');
     } else {
-        $this.parents('.panel').find('.panel-body').slideDown();
-        $this.removeClass('panel-collapsed');
-        $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+        $this.parents('.card').find('.card-block').slideDown();
+        $this.removeClass('collapsed');
+        $this.find('i').removeClass('fa-chevron-down').addClass('fa-chevron-up');
     }
 })
