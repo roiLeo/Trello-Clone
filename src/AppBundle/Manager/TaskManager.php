8f49037d8bba8f8426a22ef5861779de0aa91cc6
Nobody <medina.leo42@gmail.com>
2017-03-14 15:07:36 +0100

    init feature add (taskManager)

---
+++ src/AppBundle/Manager/TaskManager.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Utilisateur
+ * Date: 14/03/2017
+ * Time: 15:06
+ */
+
+namespace AppBundle\Manager;
+
+use Doctrine\ORM\EntityManager;
+use AppBundle\Entity\Category;
+
+
+class TaskManager
+{
+
+}
